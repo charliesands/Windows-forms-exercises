a0c14d243468b606d5473326b4cdc1c143e4284f
charliesands <cwsands3@gmail.com>
2018-11-09 19:35:39 -0700

    pool puzzle exercise done

---
+++ WindowsFormsExercises/Echo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsExercises
+{
+    class Echo
+    {
+        public int Count = 0;
+        public string Hello()
+        {
+            return "helloooo...";
+        }
+    }
+}
--- WindowsFormsExercises/Form1.cs
+++ WindowsFormsExercises/Form1.cs
@@ -16,5 +16,29 @@ namespace WindowsFormsExercises
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            Echo e1 = new Echo();
+            Echo e2 = new Echo();
+            int x = 0;
+            while (x < 4)
+            {
+                result = result + e1.Hello() + "\n";
+                e1.Count = e1.Count + 1;
+                if (x == 3)
+                {
+                    e2.Count = e2.Count + 1;
+                }
+                if (x > 0)
+                {
+                    e2.Count = e2.Count + e1.Count;
+                }
+                x = x + 1;
+            }
+            MessageBox.Show(result + "Count: " + e2.Count);
+
+        }
     }
 }
