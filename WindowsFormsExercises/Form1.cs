5f42beb461495c7fadcb7081c2e65925af90bb87
charliesands <cwsands3@gmail.com>
2018-11-15 13:54:23 -0700

    code magent exercise done

--- WindowsFormsExercises/Form1.cs
+++ WindowsFormsExercises/Form1.cs
@@ -40,5 +40,33 @@ namespace WindowsFormsExercises
             MessageBox.Show(result + "Count: " + e2.Count);
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+
+            int[] index = new int[4];
+            index[0] = 1;
+            index[1] = 3;
+            index[2] = 0;
+            index[3] = 2;
+
+            string[] islands = new string[4];
+            islands[0] = "Bermuda";
+            islands[1] = "Fiji";
+            islands[2] = "Azores";
+            islands[3] = "Cozumel";
+
+            int y = 0;
+            int refNum;
+            while (y < 4)
+            {
+                refNum = index[y];
+                result += "\nisland = ";
+                result += islands[refNum];
+                y = y + 1;
+            }
+            MessageBox.Show(result);
+        }
     }
 }
